06295a94ee102eb26712df7af8614e89a3be354a
Felipe G <pipe2705@gmail.com>
2019-10-31 15:02:45 -0700

    I added my first element it was fun

---
+++ Hogwarts/app.js
@@ -0,0 +1,12 @@
+
+let container = document.querySelector("#container"); 
+console.log(container) 
+
+
+
+let newH1 = document.createElement("h1")
+newH1.innerHTML = "Hogwarts"
+console.log(newH1)
+
+container.append(newH1);
+
